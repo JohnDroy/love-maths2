169a97cc50f49b83c5a24ea3caefb4fc5c366ae0
JohnDroy <johncdroy@gmail.com>
2021-10-05 07:20:44 +0000

    add muliplication game code

--- assets/js/script.js
+++ assets/js/script.js
@@ -30,7 +30,9 @@ let num2 = Math.floor(Math.random() * 25) + 1;
 
 if (gameType === "addition") {
     displayAdditionQuestion(num1, num2);
-}else {
+}else if (gameType === "multiply"){
+    displayMultiplyQuestion(num1, num2);
+} else {
     alert(` Unknown game type ${gameType}`);
     throw ` Unknown game type ${gameType}. Aborting!`;
 }
@@ -69,7 +71,9 @@ function calculateCorrectAnswer(){
 
      if (operator === "+") {
          return[operand1 + operand2, "addition"];
-     }else {
+     }else if(operator === "x"){
+         return [operand1 * operand2, "multiply"];
+     } else {
          alert(`unimplemented operator ${operator}`);
          throw `umiplemented operator ${operator}.Aborting!`;
      }
@@ -105,6 +109,8 @@ function displaySubtractQuestion(){
     
 }
 
-function displayMultiplyQuestion(){
-    
+function displayMultiplyQuestion(operand1, operand2){
+    document.getElementById(`operand1`).textContent = operand1;
+    document.getElementById(`operand2`).textContent = operand2;
+    document.getElementById(`operator`).textContent = "x";
 }
